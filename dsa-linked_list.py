f834802af755dd5b3ab1a589acf32eeb94c1af2e
yunfeili-tju <7994927+yunfeili-tju@user.noreply.gitee.com>
2022-04-29 10:14:21 +0800

    add a linked_list.py

---
+++ dsa-linked_list.py
@@ -0,0 +1,80 @@
+"""
+stript info
+name: linked list
+"""
+
+
+class Node(object):
+    """
+    node implementation
+    链表中的节点实现
+    """
+
+    def __init__(self, init_data):
+        self.data = init_data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, new_data):
+        self.data = new_data
+
+    def setNext(self, new_next):
+        self.next = new_next
+
+
+class UnordredList(object):
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        temp = Node(item)
+
+        temp.setNext(self.head)
+
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+if __name__ == '__main__':
+    print(__doc__)
+
+    
